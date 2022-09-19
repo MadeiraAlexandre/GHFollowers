107c28230a702755819a18f2c93d45d7933473a1
Alexandre Madeira <contact@alexandremadeira.dev>
2022-09-19 17:58:40 -0300

    Configured layout for Search view.
    Added custom views for text field and button.

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,33 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Alexandre Madeira on 19/09/22.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 12
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ GHFollowers/GFTextField.swift
@@ -0,0 +1,35 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by Alexandre Madeira on 19/09/22.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 12
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Username"
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
